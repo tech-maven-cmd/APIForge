c454e68223531d27357ce46402407d2c8e195995
Vikalpwebtech <vikalprautela@gmail.com>
2024-07-20 16:05:54 +0530

    Added Spring Project

---
+++ APIForge/src/test/java/com/tech_maven/APIForge/ApiForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech_maven.APIForge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiForgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
